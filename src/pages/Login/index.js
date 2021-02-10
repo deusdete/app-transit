c22ff0576838433f91ab32eae92be27aeada62b6
Deusdete Ferreira <deuzinh2010@gmail.com>
2021-02-10 12:01:09 -0300

    Add register

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,24 +1,37 @@
 import React, {useState} from "react";
+import { useNavigation } from '@react-navigation/native';
 import { View, Text } from "react-native";
-import { TextInput, Button } from 'react-native-paper'
+import { TextInput, Button, HelperText } from 'react-native-paper'
 import api from "../../services/api";
 
 export default function Login() {
+  const navigation = useNavigation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
 
   async function handleLogin(){
     api.post('/auth/login', {email, password}).then(res => {
-      const [ user, token ] = res.data;
+      const { user, token } = res.data;
       console.log(token)
     }).catch(err => {
-      console.log(err)
+      if(err.response){
+        setError('Email ou senha invalida')
+      }else if(err.request){
+        setError('Erro com a conexão com o servidor, tente novamente mais tarde.')
+      }else{
+        console.log(err)
+      }
+     
     })
   }
+ 
   return (
     <View>
       <Text>Login</Text>
       <View>
+        <Text type="error" visible={true} >{error}</Text>
         <TextInput
           label="Email"
           value={email}
@@ -30,6 +43,9 @@ export default function Login() {
         <Button mode="contained" onPress={handleLogin}>
           Login
         </Button>
+      </View>
+      <View>
+        <Text onPress={() => navigation.navigate('Register')} >Criar uma nova conta</Text>
       </View>
     </View>
   );
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,10 +1,56 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState} from "react";
+import { View, Text } from "react-native";
+import { TextInput, Button } from 'react-native-paper'
+import api from "../../services/api";
 
 export default function Register() {
-    return (
-        <View>
-            <Text>Registro</Text>
-        </View>
-    )
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [location, setLocation] = useState('');
+  const [type, setType] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleLogin(){
+    api.post('/auth/register', {name, age, location, type, email, password}).then(res => {
+      const [ user, token ] = res.data;
+      console.log(token)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+  return (
+    <View>
+      <Text>Login</Text>
+      <View>
+        <TextInput
+          label="Nome"
+          value={name}
+          onChangeText={ text => setName(text)} />
+        <TextInput
+          label="Idade"
+          value={age}
+          onChangeText={ text => setAge(text)} />
+        <TextInput
+          label="Endereço"
+          value={location}
+          onChangeText={ text => setLocation(text)} />
+        <TextInput
+          label="Tipo de usuário"
+          value={type}
+          onChangeText={ text => setType(text)}/>
+        <TextInput
+          label="Email"
+          value={email}
+          onChangeText={ text => setEmail(text)} />
+        <TextInput
+          label="Senha"
+          value={password}
+          onChangeText={ text => setPassword(text)} />
+        <Button mode="contained" onPress={handleLogin}>
+          Criar
+        </Button>
+      </View>
+    </View>
+  );
 }
