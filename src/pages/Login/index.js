974f8dfe3eb93b57b4aeb75aeef8ecdbc264ab78
Deusdete Ferreira <deuzinh2010@gmail.com>
2021-02-10 10:59:28 -0300

    Add login e api

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,19 @@
 import React, {useState} from "react";
 import { View, Text } from "react-native";
 import { TextInput, Button } from 'react-native-paper'
+import api from "../../services/api";
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   async function handleLogin(){
-    console.log({email,password})
+    api.post('/auth/login', {email, password}).then(res => {
+      const [ user, token ] = res.data;
+      console.log(token)
+    }).catch(err => {
+      console.log(err)
+    })
   }
   return (
     <View>
